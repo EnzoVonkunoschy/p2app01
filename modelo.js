bd47fa9b95f39bd25347169e400f7892e69532d1
Maye-3104 <166740943+Maye-3104@users.noreply.github.com>
2024-10-22 19:10:52 -0300

    funcion eliminar

--- modelo.js
+++ modelo.js
@@ -4,24 +4,24 @@ function guardarTexto(){
     fs.writeFileSync('./miTexto.txt',"Oid mortales el grito sagrado.")
 }
 
-//guardarTexto();
+guardarTexto();
 
 function leerTexto(){
     try{
     let data = fs.readFileSync('./miTexto.txt','utf-8')
     console.log(data)
     }catch (error){
-        //console.log(error.message)
+        console.log(error.message)
         console.log("Error al leer el archivo")
     }
 }
-//leerTexto()
+leerTexto()
 
 function guardarObjeto(){
     const unAfiliado = {nombre: "Enzo", edad:56, afiliado: true}
     fs.writeFileSync('./afiliado.txt',JSON.stringify(unAfiliado))
 }
-//guardarObjeto()
+guardarObjeto()
 
 function leerObjeto(){
     const strObjeto = fs.readFileSync('./afiliado.txt','utf-8')
@@ -29,12 +29,12 @@ function leerObjeto(){
     const objeto = JSON.parse(strObjeto)
     console.log(objeto)
 }
-//leerObjeto()
+leerObjeto()
 
 function guardarColeccion(col){
     fs.writeFileSync('./coleccion.txt',JSON.stringify(col))
 }
-//uardarColeccion([{nombre: "Enzo", edad: 56, afiliado: true},{nombre:"Luna", edad:30, afiliado: false}])
+guardarColeccion([{nombre: "Enzo", edad: 56, afiliado: true},{nombre:"Luna", edad:30, afiliado: false}])
 
 function leerColeccion(){
     const strColeccion = fs.readFileSync('./coleccion.txt','utf-8')
@@ -42,7 +42,7 @@ function leerColeccion(){
     const coleccion = JSON.parse(strColeccion)
     console.log(coleccion)
 }
-//leerColeccion()
+leerColeccion()
 
 function agregarElemento(elem){
     let coleccion = []
@@ -58,14 +58,33 @@ function agregarElemento(elem){
  
     fs.writeFileSync('./coleccion.txt',JSON.stringify(coleccion))
 }
-//agregarElemento({nombre: "Pedro",edad: 34, afiliado: false})
+agregarElemento({nombre: "Pedro",edad: 34, afiliado: false})
 
 function eliminarElemento(elem){
     //Leer la colección
+    let coleccion = []
+    try {
+        // Leer la colección existente
+        const strColeccion = fs.readFileSync('./coleccion.txt', 'utf-8');
+        if (strColeccion) {
+            coleccion = JSON.parse(strColeccion);
+        }
+    } catch (error) {
+        console.log("Error al leer la colección o la colección está vacía.");
+    }
+     //Filtrar elementos distintos con .filter(...)
+     coleccionFiltrada = coleccion.filter( item =>
+        !(item.nombre === elem.nombre && item.edad === elem.edad && item.afiliado === elem.afiliado )
+     );
 
-    //Filtrar elementos distintos con .filter(...)
-
-    //Guardar la colección de elementos distintos
-}
-//eliminarElemento({nombre: ...., ....})
+     try{
+        // Guardar la colección de elementos distintos
+        fs.writeFileSync('./coleccion.txt', JSON.stringify(coleccionFiltrada));
+        console.log("Elemento eliminado con Éxito")
+     }
+    
+     catch (error) {
+        console.log("Error al guardar la colección de elementos distintos.");
+     }
+    }
 
